aeccc4ff22387f26d1954c48b74437540262970c
Phila Sithole <phila.sithole1@mtn.com>
2025-04-30 17:40:17 +0200

    Testing

--- Test.php
+++ Test.php
@@ -1,1 +1,13 @@
-This is a test After the being uploaded
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<h1>This is test for git </h1>
+    
+</body>
+</html>
